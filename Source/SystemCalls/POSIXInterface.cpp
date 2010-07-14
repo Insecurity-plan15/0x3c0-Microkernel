cd8acf3a899ef9bb3dc5e537f82e41a97454ceb6
0x3c0 <edward.neal@gmail.com>
2010-07-14 09:14:39 +0100

    Simple commit, just added POSIX interrupt system's receive capability and a small mental note regarding execve and the return EIP

--- Source/SystemCalls/POSIXInterface.cpp
+++ Source/SystemCalls/POSIXInterface.cpp
@@ -15,6 +15,9 @@ SystemCallDefinition(fork)
     //Clones a process' address space
     //Only the calling thread is cloned
     //All file handles remain open
+
+    //to avoid having to copy and rebase the stack (horrid idea), I'll simply put it in the same memory address as the parent
+    //fork() will already have copied the values themselves, making this the simplest way
     return 0;
 }
 
@@ -24,6 +27,8 @@ SystemCallDefinition(execve)
     //All address space outside of heaps is wiped, and memory image loaded
     //Current threads deleted, new thread added
     //  initial values of environment, arguments, EIP, etc
+
+    //mental note: must alter return EIP, otherwise I'll page fault
     return 0;
 }
 
@@ -40,5 +45,22 @@ SystemCallInterrupt::~SystemCallInterrupt()
 
 void SystemCallInterrupt::receive(StackStates::Interrupt *stack)
 {
-    //bleh
+    unsigned int eip = stack->EIP;
+	unsigned int eax = 0;
+
+	if(stack->EDX >= (sizeof(calls) / sizeof(SystemCall)))
+	{
+		//Somebody's trying to mess up the kernel. Stop them
+		stack->EAX = 0xFFFFFFFF;
+		return;
+	}
+	SystemCall sc = calls[stack->EDX];
+
+	/*
+	* This takes into account the fact that the execve system call can change the values in the stack. If I change the new stack state,
+	* it'll be inconsistent, and in this case the return address will go wobbly.
+	*/
+	eax = (*sc)(stack->EAX, stack->EBX, stack->ECX, stack);
+	if(stack->EIP == eip)
+		stack->EAX = eax;
 }
